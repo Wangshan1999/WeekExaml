d4623d3dec87321f388232857c25148ad082f005
Wangshan1999 <1445083817@qq.com>
2019-01-07 19:27:56 +0800

    xiangqi

---
+++ guoji.php
@@ -0,0 +1,18 @@
+<?php 
+echo "<table border='1px' width='500px';bordercolor='black'>";
+for ($i=1; $i <=10 ; $i++) { 
+	echo "<tr>";
+	for ($j=1; $j <=10 ; $j++) { 
+		if (($i+$j)%2) {
+			echo "<td bgcolor='black' width='50px' height='50px'>";
+		}
+		else
+		{
+			echo "<td></td>";
+		}
+	}
+	echo "</tr>";
+}
+
+
+ ?>
